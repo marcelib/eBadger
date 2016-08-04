6ffc9a8293337d2de27057e75097f5ea22e50205
Marcelele <marceli.baczewski@gmail.com>
2016-08-04 08:06:47 +0200

    Add basic project structure and gradle configuration file.

---
+++ src/main/java/com/ebadger/EBadgerApplication.java
@@ -0,0 +1,12 @@
+package com.ebadger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBadgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBadgerApplication.class, args);
+    }
+}
